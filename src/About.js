b99dce841ce77d605ef3e03a759034ce66b4bafd
sahildkun <sahilsahoo23@gmail.com>
2023-01-06 00:39:06 +0530

    s

--- src/About.js
+++ src/About.js
@@ -13,7 +13,7 @@ function About() {
       <p className=' text-sm md:text-2xl  text-justify'>
         SEVA and SHELTER CHARITABLE TRUST- Living away from an loving family can be really harsh. But coping with the demands of a fast paced life is not easy in the twilight years. To find a middle way JRP and MasterPace has started a peace home for our Senior Citizen called "SEVA AND SHELTER CHARITABLE TRUST".
         </p>
-      <p className='text-sm md:text-2xl   text-justify'>
+      <p className='text-sm md:text-2xl   text-start'>
         These days many senior citizens are opting for the old "PAY & STAY" facilities to live securely and spend the rest  of their lifetime in peace and harmony. SEVA & SHELTER CHARITABLE TRUST is probably the best facility in BHUBANESWAR,ODISHA to offer older a brief stay
         </p>
         <div className='text-sm md:text-2xl  text-center flex flex-col'>
