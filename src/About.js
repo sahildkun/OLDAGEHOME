84c620c6ad63717ba115c3aaa13f93c7479371c8
sahildkun <sahilsahoo23@gmail.com>
2023-01-06 18:56:46 +0530

    c

--- src/About.js
+++ src/About.js
@@ -7,17 +7,19 @@ function About() {
      
      <div className=' mx-5 p-5 md:mx-16 md:p-10 flex flex-col space-y-7'>
 
-      <h1 className='text-5xl font-serif text-center'>
-        ABOUT US
+      <h1 className='text-7xl font-serif text-center text-blue-800 underline' id='about'>
+        About us
       </h1>
-      <p className=' text-sm md:text-2xl  text-justify'>
-        SEVA and SHELTER CHARITABLE TRUST- Living away from an loving family can be really harsh. But coping with the demands of a fast paced life is not easy in the twilight years. To find a middle way JRP and MasterPace has started a peace home for our Senior Citizen called "SEVA AND SHELTER CHARITABLE TRUST".
+      <div id='text' >
+      <p className=' text-sm md:text-2xl text-slate-700 font-semibold text-justify'>
+        SEVA and SHELTER CHARITABLE TRUST- Living away from a loving family can be really harsh. But coping with the demands of a fast paced life is not easy in the twilight years. To find a middle way JRP and MasterPace has started a peace home for our Senior Citizens called "SEVA AND SHELTER CHARITABLE TRUST".
         </p>
-      <p className='text-sm md:text-2xl   text-start'>
+      <p className='text-sm md:text-2xl my-5 text-slate-700 font-semibold text-start'>
         These days many senior citizens are opting for the old "PAY & STAY" facilities to live securely and spend the rest  of their lifetime in peace and harmony. SEVA & SHELTER CHARITABLE TRUST is probably the best facility in BHUBANESWAR,ODISHA to offer older a brief stay
         </p>
-        <div className='text-sm md:text-2xl  text-center flex flex-col'>
-        <p className=''>if you or anay of your relatives intrested to live in a peaceful stay visit</p> <a href='https://goo.gl/maps/DbrND1AucTPbXzg8A' className='text-blue-700  hover:text-red-700 hover:underline'>SEVA AND SHELTER CHARITABLE TRUST</a> <p>OR</p>  CALL US AT <a href="tel:+919556100564" className='text-blue-700  hover:text-red-700 hover:underline'>9556100564</a>
+        <div className='text-sm md:text-2xl font-semibold mt-10 text-center flex flex-col'>
+        <p className=''>If you or any of your relatives intrested to live in a peaceful stay visit</p> <a href='https://goo.gl/maps/DbrND1AucTPbXzg8A' className='text-blue-700  hover:text-red-700 hover:underline'>SEVA AND SHELTER CHARITABLE TRUST</a> <p>OR</p>  CALL US AT <a href="tel:+919556100564" className='text-blue-700  hover:text-red-700 hover:underline'>9556100564</a>
+        </div>
         </div>  
      </div>
     </>
--- src/Hero.js
+++ src/Hero.js
@@ -5,6 +5,7 @@ import Footer2 from './components/Footer2'
 import Founder from './components/Founder'
 import Navbar from './components/Navbar'
 import Services from './Services'
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
@@ -17,10 +18,10 @@ function Hero() {
        <div className='flex flex-col items-center mt-8 mb-8'>
 
          <h1 className='pb-3  md:text-lg text-bold font-serif text-slate-600'>OLD AGE CARE </h1>
-         <h1 className='text-xl text-center md:text-5xl text-bold font-serif mb-5 text-[#110364]'>SEVA AND SHELTER CHARITABLE TRUST </h1>
+         <h1 className='text-xl text-center md:text-5xl text-bold  mb-5 text-[#110364]' id='title'>SEVA AND SHELTER CHARITABLE TRUST </h1>
          <h1 className='text-xs mx-auto md:text-lg max-w-xl text-center font-thin mb-10 text-slate-700 md:mt-5 drop-shadow-2xl'> SEVA & SHELTER, is a charitable trust founded in the year 2001 with the objective of providing care and succor to the old. Seva and Shelter charitable trust provides residential care with full boarding & lodging facility to the old with medical...</h1>
-         <button  className='text-sm p-2 md:mx-4 my-6 bg-gradient-to-br from-[#04094b] to-[#04094b] md:px-5 md:py-3 md:space-x-3 rounded-md text-white 
-         hover:from-[#5f5de9] hover:to-[#5f5de9] drop-shadow-md'>LEARN MORE</button>
+       <Link to={'/about'}><button  className='text-sm p-2 md:mx-4 my-6 bg-gradient-to-br from-[#04094b] to-[#04094b] md:px-5 md:py-3 md:space-x-3 rounded-md text-white 
+         hover:from-[#5f5de9] hover:to-[#5f5de9] drop-shadow-md'>LEARN MORE</button></Link>
         
        </div>
     </section>
